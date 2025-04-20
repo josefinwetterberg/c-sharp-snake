5c26ad1d1bda588f8ace53da468f600f519f141f
Josefinx <josefin.wetterberg@gmail.com>
2025-04-20 22:59:40 +0200

    Continued snake logic

--- c-sharp-snake/Program.cs
+++ c-sharp-snake/Program.cs
@@ -1,5 +1,7 @@
 ﻿using Snake;
 
+int score = 0;
+
 Coord gridDimensions = new Coord(70, 30);
 Coord snakePosition = new Coord(10, 1);
 
@@ -10,10 +12,14 @@ int frameDelayInMilliseconds = 100;
 
 Direction movementDirection = Direction.Down;
 
+List<Coord> snakePositionHistory = new List<Coord>();
+int tailLength = 1;
 
 while (true)
 {
     Console.Clear();
+    Console.WriteLine("Score: " + score);
+
     snakePosition.ApplyMovementDirection(movementDirection);
     
     for (int y = 0; y < gridDimensions.Y; y++)
@@ -21,20 +27,71 @@ while (true)
         for (int x = 0; x < gridDimensions.X; x++)
         {
             Coord currentCoord = new Coord(x, y);
-            
-            if (snakePosition.Equals(currentCoord))
+
+            if (snakePosition.Equals(currentCoord) || snakePositionHistory.Contains(currentCoord))
                 Console.Write('■');
-            
+
             else if (applePosition.Equals(currentCoord))
-                Console.Write("a");
-            
+            {
+                Console.ForegroundColor = ConsoleColor.DarkRed;
+                Console.Write("@");
+                Console.ResetColor();
+            }
             else if (x == 0 || y == 0 || x == gridDimensions.X - 1 || y == gridDimensions.Y - 1)
                 Console.Write("#");
-            
+
             else
                 Console.Write(" ");
         }
         Console.WriteLine();
     }
-    Thread.Sleep(frameDelayInMilliseconds);
+    
+    if (snakePosition.Equals(applePosition))
+    {
+        tailLength++;
+        score++;
+        applePosition = new Coord(random.Next(1, gridDimensions.X - 1), random.Next(1, gridDimensions.Y - 1));
+    }
+    else if (snakePosition.X == 0 || snakePosition.Y == 0 || snakePosition.X == gridDimensions.X - 1 || snakePosition.Y == gridDimensions.Y - 1 || snakePositionHistory.Contains(snakePosition))
+    {
+        score = 0;
+        tailLength = 1;
+        snakePosition = new Coord(10, 1);
+        snakePositionHistory.Clear();
+        movementDirection = Direction.Down;
+        continue;
+    }
+    
+    snakePositionHistory.Add(new Coord(snakePosition.X, snakePosition.Y));
+ 
+    if (snakePositionHistory.Count > tailLength)
+        snakePositionHistory.RemoveAt(0);
+
+    
+    DateTime time = DateTime.Now;
+    
+    while((DateTime.Now - time).Milliseconds < frameDelayInMilliseconds)
+    {
+        if (Console.KeyAvailable)
+        {
+            ConsoleKey key = Console.ReadKey().Key;
+
+            switch (key)
+            {
+                case ConsoleKey.LeftArrow:
+                    movementDirection = Direction.Left;
+                    break;
+                case ConsoleKey.RightArrow:
+                    movementDirection = Direction.Right;
+                    break;
+                case ConsoleKey.UpArrow:
+                    movementDirection = Direction.Up;
+                    break;
+                case ConsoleKey.DownArrow:
+                    movementDirection = Direction.Down;
+                    break;
+            }
+            
+        }
+    }
 }
