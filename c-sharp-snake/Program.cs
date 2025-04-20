df2e799ce16ebe9ed2aa337cc0d836cc950a7bb2
Josefinx <josefin.wetterberg@gmail.com>
2025-04-20 22:17:52 +0200

    Added Coord-class and started snake structure.

--- c-sharp-snake/Program.cs
+++ c-sharp-snake/Program.cs
@@ -1,3 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Snake;
 
-Console.WriteLine("Hej!");
+Coord gridDimensions = new Coord(70, 30);
+Coord snakePosition = new Coord(10, 1);
+
+Random random = new Random();
+
+Coord applePosition = new Coord(random.Next(1, gridDimensions.X-1), random.Next(1, gridDimensions.Y-1));
+Coord applePos = new Coord(random.Next(1, gridDimensions.X - 1), random.Next(1, gridDimensions.Y - 1));
+
+
+for (int y = 0; y < gridDimensions.Y; y++)
+{
+    for (int x = 0; x < gridDimensions.X; x++)
+    {
+        Coord currentCoord = new Coord(x, y);
+        
+        if (snakePosition.Equals(currentCoord))
+            Console.Write('■');
+        
+        else if (applePosition.Equals(currentCoord))
+            Console.Write("a");
+        
+        else if (x == 0 || y == 0 || x == gridDimensions.X - 1 || y == gridDimensions.Y - 1)
+            Console.Write("#");
+        
+        else
+            Console.Write(" ");
+    }
+    Console.WriteLine();
+}
